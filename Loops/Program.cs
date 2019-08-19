ce7ee5b3b55f2c25a300c120633d6906a1df3553
Blake Smith <s198050@students.aie.edu>
2019-08-19 16:06:38 -0500

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int count = 0;
+
+            //while (count < 3)
+            //{
+            //    count++;
+            //    Console.WriteLine("The loop has run " + count + " times.");
+            //}
+            //Console.ReadKey();
+            for( int count = 3; count >= 0; count -= 1)
+            {
+                Console.WriteLine("The value of count is now  " + count + ".");
+                Console.ReadKey();
+                if (count == 2)
+                {
+                    //break;
+                    return;
+                }
+            }
+            Console.WriteLine("The loop is now finished.");
+            Console.ReadKey();
+        }
+    }
+}
